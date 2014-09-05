cf25c81f31295e680d790734865384684c7c3c12
Zachary Sailer <zachsailer@gmail.com>
2014-09-05 16:31:26 -0700

    Functional networks now

--- network_viewer/js/network.js
+++ network_viewer/js/network.js
@@ -18,9 +18,9 @@ var Network = function (selector) {
             .attr("transform", "translate(40,0)");
     
     this.force = d3.layout.force()
-    .charge(-1)
-    .linkDistance(20)
-    .size([this.width, this.height]);
+    .charge(-60)
+    .linkDistance(100)
+    .size([this.width-10, this.height-10]);
             
 };
 
@@ -34,17 +34,33 @@ Network.prototype.build_network = function(graph) {
         .links(graph.links)
         .start();
         
-    var link = svg.selectAll(".link")
+    var link = this.svg.selectAll(".link")
         .data(graph.links)
         .enter().append("line")
         .attr("class", "link")
-        .style("stroke_width", 5); // Change width later
+        .style("stroke_width", 10);         // Change width later
         
-    var node = svg.selectAll(".node")
+    var node = this.svg.selectAll(".node")
         .data(graph.nodes)
-        .enter().append("circles")
-        attr("class", "link")
-        .style("fill", "black");
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 5)
+        .style("fill", "#111")
+        .call(this.force.drag);
+    
+    this.force.on("tick", function () {
         
-        
+        link 
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+            
+        node   
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+    });
+
+    this.link = link;
+    this.node = node;
 };
